f62b146a3b77247565cd68344fc4bf38484828d4
Piotr Kosiński <piotrkosinski@gmail.com>
2018-11-22 15:12:03 +0100

    boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Button } from "carbon-components-react";
+import * as React from "react";
+import "./App.css";
+
+import logo from "./logo.svg";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button>Hello React!</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
