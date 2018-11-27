b2b2570400d53bc5dcb6cbe68c6f251fd7c0c23f
Piotr Kosiński <piotrkosinski@gmail.com>
2018-11-27 14:16:46 +0100

    Sentinel ID, Icon

--- src/App.tsx
+++ src/App.tsx
@@ -84,9 +84,15 @@ class App extends React.Component<{}, ISentinelState> {
         ''
       );
 
+    const icon = (
+      <svg width="21" height="24" viewBox="0 0 21 24">
+        <path d="M9.5 1c.9 0 1.5.7 1.5 1.5S10.4 4 9.5 4C8.7 4 8 3.3 8 2.5S8.7 1 9.5 1zm0-1C8.2 0 7 1.1 7 2.5S8.2 5 9.5 5 12 3.9 12 2.5 10.9 0 9.5 0zM17 4l-3 1.7v2.4L10.3 6 5 9v3.6l-2-1.1-3 1.7v3.5l3 1.7 3-1.7v-1.1l4.5 2.4 5-3v-4.9l1.5.9 3-1.7V5.8L17 4zm-6.7 3.2l3.8 2.1-3.5 1.8-4-1.8 3.7-2.1zM5 16.1l-2 1.2-2-1.1v-2.3l2-1.2 2 1.1v2.3zm1-5.9l4 1.8v4.6l-4-2.2v-4.2zm5 6.3V12l3.5-1.8v4.2L11 16.5zm8-7.8l-2 1.2-2-1.2V6.3l2-1.2 2 1.2v2.4zM2.5 4C3.4 4 4 4.7 4 5.5S3.4 7 2.5 7 1 6.3 1 5.5 1.7 4 2.5 4zm0-1C1.1 3 0 4.1 0 5.5S1.1 8 2.5 8 5 6.9 5 5.5 3.9 3 2.5 3zm9 17c.8 0 1.5.7 1.5 1.5s-.6 1.5-1.5 1.5-1.5-.7-1.5-1.5.7-1.5 1.5-1.5zm0-1C10.1 19 9 20.1 9 21.5s1.1 2.5 2.5 2.5 2.5-1.1 2.5-2.5-1.1-2.5-2.5-2.5zm7-3c.8 0 1.5.7 1.5 1.5s-.7 1.5-1.5 1.5-1.5-.7-1.5-1.5.7-1.5 1.5-1.5zm0-1c-1.4 0-2.5 1.1-2.5 2.5s1.1 2.5 2.5 2.5 2.5-1.1 2.5-2.5-1.1-2.5-2.5-2.5z" />
+      </svg>
+    );
+
     return (
       <div className="bx--grid">
-        <div className="bx--type-alpha bx--row">Sentinel</div>
+        <div className="bx--type-alpha bx--row">Sentinel {icon}</div>
         <div className="bx--row space" />
         <Form>
           <div className="bx--row row-padding">
@@ -138,7 +144,7 @@ class App extends React.Component<{}, ISentinelState> {
           <div className="bx--row row-padding">
             <TextInput
               className="bx--col-xs-6"
-              labelText="Conditional asset amount (transfer when balance >= condition)"
+              labelText="Conditional asset amount (transfer when balance >= condition) [transaction amount when empty]"
               value={this.state.conditionAmount}
               required={true}
               // tslint:disable-next-line:jsx-no-lambda
@@ -231,9 +237,10 @@ class App extends React.Component<{}, ISentinelState> {
 
   private async send() {
     const dec = new BigNumber(10).pow(this.state.conditionAssetDecimals);
-    const conditionAmount = new BigNumber(this.state.conditionAmount)
-      .mul(dec)
-      .toString();
+    const conditionAmount =
+      this.state.conditionAmount !== ''
+        ? new BigNumber(this.state.conditionAmount).mul(dec).toString()
+        : this.state.signedAmount;
 
     const payload = {
       conditionAmount,
