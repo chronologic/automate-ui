7b21b3fcfdfa05f7a2e2ea01a1b38b8a5176441f
Piotr Kosiński <piotrkosinski@gmail.com>
2018-12-14 11:19:47 +0100

    Add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 
 class Header extends React.Component {
   public render() {
     return (
       <div className="bx--row">
         <Link to="/" style={{ textDecoration: 'none', display: 'flex' }}>
-          <img
-            src="https://s3.amazonaws.com/chronologic.network/ChronoLogic_logo.svg"
-            width="128px"
-            height="48px"
-          />
-          <div className="bx--type-alpha bx--col-xs-3">Automate</div>
+          <img src={logo}/>
         </Link>
       </div>
     );
