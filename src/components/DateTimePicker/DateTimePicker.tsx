e5910b769139b2ad8317ee25f6f5c1ae20778bc5
Piotr Kosiński <piotrkosinski@gmail.com>
2019-02-09 18:56:42 +0100

    Fix tz transformations

--- src/components/DateTimePicker/DateTimePicker.tsx
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -16,19 +16,24 @@ interface IDateTimePickerView {
 }
 
 interface IDateTimePickerState {
-  date?: number;
-  timeInvalid: boolean;
+  combined?: number;
+  day?: number;
   dateInvalid: boolean;
-  minutes?: number;
   hours?: number;
+  minutes?: number;
+  month?: number;
+  timeInvalid: boolean;
   tz: string;
+  year?: number;
 }
 
 class DateTimePicker extends React.Component<
   IDateTimePickerView,
   IDateTimePickerState
 > {
-  private timePattern = new RegExp('^(([0-1]{0,1}[0-9])|(2[0-3])):[0-5]{0,1}[0-9]$');
+  private timePattern = new RegExp(
+    '^(([0-1]{0,1}[0-9])|(2[0-3])):[0-5]{0,1}[0-9]$'
+  );
 
   constructor(props: any) {
     super(props);
@@ -45,39 +50,43 @@ class DateTimePicker extends React.Component<
 
     return (
       <div className="bx--row row-padding">
-      <div className="bx--col-xs-2">
-        <DatePicker
-          id="date-picker"
-          datePickerType="single"
-          dateFormat="d/m/Y"
-          onChange={onDateChange}
-        >
-          <DatePickerInput
-            id="date-picker-input"
-            labelText="Date"
-            iconDescription="description"
-            placeholder="dd/mm/yyyy"
-            invalid={this.state.dateInvalid}
-            invalidText="Date in the past"
-            disabled={this.props.disabled}
-          />
-        </DatePicker>
+        <div className="bx--col-xs-2">
+          <DatePicker
+            id="date-picker"
+            datePickerType="single"
+            dateFormat="d/m/Y"
+            onChange={onDateChange}
+          >
+            <DatePickerInput
+              id="date-picker-input"
+              labelText="Date"
+              iconDescription="description"
+              placeholder="dd/mm/yyyy"
+              invalid={this.state.dateInvalid}
+              invalidText="Date in the past"
+              disabled={this.props.disabled}
+            />
+          </DatePicker>
         </div>
         <div className="bx--col-xs-3">
-        <TimePicker
-          id="time-picker"
-          labelText="Time"
-          maxLength={5}
-          placeholder="hh:mm"
-          pattern=""
-          // tslint:disable-next-line:jsx-no-lambda
-          onChange={(e: any) => onTimeChange(e.target.value)}
-          invalid={this.state.timeInvalid}
-          invalidText="Incorrect time"
-          disabled={this.props.disabled}
-        >
-          {this.renderTimezonePicker()}
-        </TimePicker>
+          <TimePicker
+            id="time-picker"
+            labelText="Time"
+            maxLength={5}
+            placeholder="hh:mm"
+            pattern=""
+            // tslint:disable-next-line:jsx-no-lambda
+            onChange={(e: any) => onTimeChange(e.target.value)}
+            invalid={this.state.timeInvalid}
+            invalidText="Incorrect time"
+            disabled={this.props.disabled}
+          >
+            {this.renderTimezonePicker()}
+          </TimePicker>
+        </div>
+        <div className="bx--col-xs-3">
+          Local time: <br/>
+          {this.state.combined ? moment(this.state.combined).format('DD/MM/YYYY HH:mm') : ""}
         </div>
       </div>
     );
@@ -107,35 +116,43 @@ class DateTimePicker extends React.Component<
     if (!isNaN(timeStamp)) {
       this.props.onChange(timeStamp, tz);
     } else {
-      this.props.onValidationError("Wrong date");
+      this.props.onValidationError('Wrong date');
     }
   }
 
   private onTimezoneChange(tz: string) {
-    this.setState({ tz });
-
     const combined = this.combine(
-      this.state.date!,
+      this.state.year!,
+      this.state.month!,
+      this.state.day!,
       this.state.hours!,
       this.state.minutes!,
       tz
-    );
-
+      );
+      
+    
+    this.setState({ combined, tz });
+    
     this.tryEmitResult(combined, tz);
   }
 
   private onDateChange(dates: Date[]) {
-    const ts = dates[0].getTime();
-        
+    const date = moment(dates[0]);
+    const year = date.year();
+    const month = date.month();
+    const day = date.date();
+
     const combined = this.combine(
-      ts,
+      year,
+      month,
+      day,
       this.state.hours!,
       this.state.minutes!,
       this.state.tz
-      );
-      
+    );
+
     const isAfter = moment().isAfter(combined);
-    this.setState({ dateInvalid: isAfter, date: ts });
+    this.setState({ combined, dateInvalid: isAfter, year, month, day });
 
     this.tryEmitResult(combined, this.state.tz);
   }
@@ -150,29 +167,34 @@ class DateTimePicker extends React.Component<
 
     const hours = parsed.hours();
     const minutes = parsed.minutes();
-  
+
     const combined = this.combine(
-      this.state.date!,
+      this.state.year!,
+      this.state.month!,
+      this.state.day!,
       hours,
       minutes,
       this.state.tz
     );
 
     const isAfter = moment().isAfter(combined);
-    this.setState({ dateInvalid: isAfter, timeInvalid: !isValid, hours, minutes });
+    this.setState({
+      combined,
+      dateInvalid: isAfter,
+      hours,
+      minutes,
+      timeInvalid: !isValid
+    });
 
     this.tryEmitResult(combined, this.state.tz);
   }
 
-  private combine(date: number, hours: number, minutes: number, tz: string) {
-    if (!date || isNaN(hours) || isNaN(minutes)) {
+  private combine(year: number, month: number, day: number, hours: number, minutes: number, tz: string) {
+    if (!year || !month || !day || isNaN(hours) || isNaN(minutes)) {
       return NaN;
     }
 
-    const result = moment
-      .tz(date, tz)
-      .hours(hours)
-      .minutes(minutes);
+    const result = moment.tz({year, month, day, hours, minutes}, tz)
 
     return result.unix() * 1000;
   }
