0afeff52adcda6335867c36f3fcd0434d847eb8a
Marcin <zolkiewski.marcin@gmail.com>
2021-03-16 10:34:34 +0100

    added auth route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import 'antd/dist/antd.css';
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Auth from './components/Auth/Auth';
 import Header from './components/Header/Header';
 import Schedule from './components/Schedule/Schedule';
 import Scheduleds from './components/Scheduleds';
@@ -31,10 +32,7 @@ class App extends React.Component<{}, IAppState> {
   public render() {
     const { updateCounter } = this.state;
     const renderScheduleRoute = () => <Schedule onChange={this.handleChange} />;
-    const renderViewRoute = (props: any) => (
-      <View {...props} onChange={this.handleChange} />
-    );
-    const renderScheduledsRoute = () => <Scheduleds />;
+    const renderViewRoute = (props: any) => <View {...props} onChange={this.handleChange} />;
 
     return (
       <Router>
@@ -42,7 +40,8 @@ class App extends React.Component<{}, IAppState> {
           <Header updateCounter={updateCounter} />
           <Route exact={true} path="/" render={renderScheduleRoute} />
           <Route path="/view/:id/:key" render={renderViewRoute} />
-          <Route path="/scheduleds" render={renderScheduledsRoute} />
+          <Route path="/scheduleds" component={Scheduleds} />
+          <Route path="/auth" component={Auth} />
         </div>
       </Router>
     );
