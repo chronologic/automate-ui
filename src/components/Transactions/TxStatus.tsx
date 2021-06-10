738cb79ef3d4ed2760803380b4d9b1804e1de97d
Marcin <zolkiewski.marcin@gmail.com>
2021-06-10 20:43:24 +0200

    improved TxStatus

--- src/components/Transactions/TxStatus.tsx
+++ src/components/Transactions/TxStatus.tsx
@@ -1,78 +1,72 @@
-import { useMemo } from 'react';
+import { Tooltip } from 'antd';
 import {
   CheckCircleOutlined,
   ClockCircleOutlined,
   CloseSquareOutlined,
   DeleteOutlined,
   FormOutlined,
+  WarningOutlined,
 } from '@ant-design/icons';
-import styled from 'styled-components';
 
 interface IProps {
   status: string;
   txHash: string;
 }
 
-function TxStatus({ status, txHash }: IProps) {
-  const node = useMemo(() => {
-    let res = <span />;
-    switch (status) {
-      case 'Draft': {
-        res = (
-          <span style={{ color: '#0650ff' }} title={status}>
-            <FormOutlined />
-          </span>
-        );
-        break;
-      }
-      case 'Pending': {
-        res = (
-          <span style={{ color: 'orange' }} title={status}>
-            <ClockCircleOutlined />
-          </span>
-        );
-        break;
-      }
-      case 'Completed': {
-        res = (
-          <span style={{ color: 'green' }} title={status}>
-            <CheckCircleOutlined />
-          </span>
-        );
-        break;
-      }
-      case 'Cancelled': {
-        res = (
-          <span style={{ color: 'gray' }} title={status}>
-            <DeleteOutlined />
-          </span>
-        );
-        break;
-      }
-      case 'Error': {
-        res = (
-          <span style={{ color: 'red' }} title={status}>
-            <CloseSquareOutlined />
-          </span>
-        );
-        break;
-      }
-      default: {
-        res = <span title={status}>{status}</span>;
-        break;
-      }
-    }
-
-    return res;
-  }, [status]);
-
-  return (
-    <A href={`https://etherscan.io/tx/${txHash}`} target="_blank" rel="noopener noreferrer">
-      {node}
-    </A>
-  );
+interface IStatusConfig {
+  [key: string]: {
+    color: string;
+    description: string;
+    icon: React.ReactNode;
+  };
 }
 
-const A = styled.a``;
+const statusConfig: IStatusConfig = {
+  Draft: {
+    color: '#0650ff',
+    description: 'Draft - you need to edit this transaction before Automate starts monitoring it',
+    icon: <FormOutlined />,
+  },
+  Pending: {
+    color: 'orange',
+    description: 'Pending - Automate will execute this transaction as soon as all conditions are met',
+    icon: <ClockCircleOutlined />,
+  },
+  Completed: {
+    color: 'green',
+    description: 'Completed - transaction has executed successfully',
+    icon: <CheckCircleOutlined />,
+  },
+  Cancelled: {
+    color: 'gray',
+    description: "Cancelled - you've cancelled this transaction and Automate will no longer try to execute it",
+    icon: <DeleteOutlined />,
+  },
+  StaleNonce: {
+    color: 'pink',
+    description:
+      "StaleNonce - the transaction's nonce is lower than the last nonce on the Ethereum network. This means the transaction can not be executed. You need to schedule a new transaction with a higher nonce.",
+    icon: <WarningOutlined />,
+  },
+  Error: {
+    color: 'red',
+    description: 'Error - an error occured while executing this transaction',
+    icon: <CloseSquareOutlined />,
+  },
+};
+
+function TxStatus({ status }: IProps) {
+  const cfg = statusConfig[status];
+
+  if (cfg) {
+    return (
+      <Tooltip placement="right" title={cfg.description}>
+        <span style={{ color: cfg.color }}>{cfg.icon}</span>
+      </Tooltip>
+    );
+  }
+
+  return <span title={status}>{status}</span>;
+}
 
 export default TxStatus;
