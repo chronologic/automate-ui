f7f798bb274cd17cd43c449256b4fb4ce0e82e76
Marcin <zolkiewski.marcin@gmail.com>
2021-06-14 11:34:14 +0200

    improved error handling

--- src/api/withErrorHandler.ts
+++ src/api/withErrorHandler.ts
@@ -4,7 +4,12 @@ export function withErrorHandler<T extends Function>(fn: T): T {
   return function inner(...args: any[]) {
     return fn(...args).catch((e: any) => {
       notification.error({
-        message: e.response?.data?.error?.message || e.message || 'Error',
+        message:
+          e.response?.data?.error?.message ||
+          e.response?.data?.message ||
+          e.response?.data?.error ||
+          e.message ||
+          'Error',
       });
       throw e;
     });
