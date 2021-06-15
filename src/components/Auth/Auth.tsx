a3f0a18fa4f571c6ad9b84e9eb3101e52a2e7475
Marcin <zolkiewski.marcin@gmail.com>
2021-06-15 11:33:15 +0200

    enabled signups

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button, Input, Typography, Form } from 'antd';
 import styled from 'styled-components';
+import { parseUrl } from 'query-string';
 
 import PageTitle from '../PageTitle';
 import { useAuth } from '../../hooks';
@@ -11,10 +12,12 @@ const emailRegex =
   // eslint-disable-next-line no-control-regex
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 
+const parsed = parseUrl(window.location.href);
+
 function Auth() {
   const history = useHistory();
   const { authenticating, isAuthenticated, onAuthenticate } = useAuth();
-  const [signup, setSignup] = useState(false);
+  const [signup, setSignup] = useState(ALLOW_SIGNUP && !!parsed.query?.utm_source);
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
@@ -36,7 +39,7 @@ function Auth() {
 
   useEffect(() => {
     if (isAuthenticated) {
-      history.push('/connect');
+      history.replace('/connect');
     }
   }, [history, isAuthenticated]);
 
