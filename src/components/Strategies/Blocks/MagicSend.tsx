4c62296d8ce22ade39b65faf7b89f5f4a14bd4c7
Marcin <zolkiewski.marcin@gmail.com>
2022-04-30 00:46:11 +0200

    update tx value for magic send

--- src/components/Strategies/Blocks/MagicSend.tsx
+++ src/components/Strategies/Blocks/MagicSend.tsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Col, Typography, Input, Form } from 'antd';
 import { ExportOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import Web3 from 'web3';
 
+import ERC20ABI from '../../../abi/ERC20.json';
+import { useStrategyStore } from '../../../hooks';
 import { StrategyBlock } from '../../../constants';
 import BaseBlock from './BaseBlock';
 
 const { Text } = Typography;
+const web3 = new Web3();
+
+const MAGIC_ADDRESS = '0x539bdE0d7Dbd336b79148AA742883198BBF60342';
+const MAGIC_DECIMAL_UNIT = 'ether';
+const magicContract = new web3.eth.Contract(ERC20ABI as any);
 
 function MagicSend() {
+  const setTx = useStrategyStore((state) => state.setTx);
+  const [address, setAddress] = useState('');
+  const [amount, setAmount] = useState('');
+
+  useEffect(() => {
+    try {
+      const callData = magicContract.methods
+        .transfer(address, web3.utils.toWei(amount, MAGIC_DECIMAL_UNIT))
+        .encodeABI();
+
+      setTx(StrategyBlock.Arbitrum_Magic_Send, {
+        to: MAGIC_ADDRESS,
+        data: callData,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }, [address, amount, setTx]);
+
   return (
     <Container>
       <BaseBlock
@@ -19,13 +47,13 @@ function MagicSend() {
         }
       >
         <Col flex="390px">
-          <Form.Item name={StrategyBlock.Arbitrum_Magic_Send} normalize={getNormalizedValue} required>
-            <Input size="large" placeholder="To address" />
+          <Form.Item name="address" required>
+            <Input size="large" placeholder="To address" onChange={(e) => setAddress(e.target.value)} />
           </Form.Item>
         </Col>
         <Col flex="auto">
-          <Form.Item required>
-            <Input size="large" placeholder="Amount" />
+          <Form.Item name="amount" required>
+            <Input size="large" placeholder="Amount" onChange={(e) => setAmount(e.target.value)} />
           </Form.Item>
         </Col>
       </BaseBlock>
@@ -34,10 +62,5 @@ function MagicSend() {
 }
 
 const Container = styled.div``;
-
-function getNormalizedValue(...args: any[]) {
-  console.log(args);
-  return {};
-}
 
 export default MagicSend;
