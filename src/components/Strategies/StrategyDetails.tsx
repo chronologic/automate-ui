dcc901e9ade2e39e107a437fb7e062dffbf8da4b
AndreyDrozdov <ovalab@yandex.ru>
2022-05-27 12:09:53 +0200

    popup removed

--- src/components/Strategies/StrategyDetails.tsx
+++ src/components/Strategies/StrategyDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Row, Col, Typography, Button, Space, Form, Modal, notification } from 'antd';
+import { Row, Col, Typography, Button, Space, Form, notification } from 'antd';
 import { ArrowDownOutlined } from '@ant-design/icons';
 import ReactPlayer from 'react-player';
 import styled from 'styled-components';
@@ -21,7 +21,7 @@ import { strategies } from './strategyData';
 import { blockConfig, Repeat } from './Blocks';
 import SigningPopup from './SigningPopup';
 
-const { Title, Text, Link } = Typography;
+const { Title, Text } = Typography;
 const web3 = new Web3(ethereum as any);
 
 function StrategyDetails() {
@@ -37,8 +37,6 @@ function StrategyDetails() {
   const [displaySigningPopup, setDisplaySigningPopup] = useState(false);
   const [currentTxIndex, setCurrentTxIndex] = useState(-1);
   const [completedSigning, setCompletedSigning] = useState(false);
-  const [modal2Visible, setModal2Visible] = useState(false);
-  const [play] = useState(true);
 
   const strategyName = useMemo(() => {
     return location?.pathname?.split('/').reverse()[0];
@@ -143,18 +141,7 @@ function StrategyDetails() {
             </Text>
             <br />
             <br />
-            <Link onClick={() => setModal2Visible(true)}>Watch Tutorial</Link>
-            <Modal
-              title="How to sign up to Automate and claim your $MAGIC✨ rewards"
-              centered
-              width={590}
-              footer={null}
-              visible={modal2Visible}
-              destroyOnClose={true}
-              onCancel={() => setModal2Visible(false)}
-            >
-              <ReactPlayer url="https://youtu.be/IKcruLqdTSs" width="100%" controls playing={play} />
-            </Modal>
+            <ReactPlayer url="https://youtu.be/IKcruLqdTSs" width="540px" controls />
           </Col>
           <Col span={24} lg={12}>
             <div className="outer">
