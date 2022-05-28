1f935d1f9a41c7aba1e4aaac91c102dcc9fda6ae
mzolkiewski <zolkiewski.marcin@gmail.com>
2022-05-28 21:43:17 +0200

    working payment modal

--- src/components/Header/BuyCreditsModal.tsx
+++ src/components/Header/BuyCreditsModal.tsx
@@ -1,39 +1,62 @@
 import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { QuestionCircleOutlined, ShoppingCartOutlined } from '@ant-design/icons';
-import { Typography, Tooltip, Modal, Form, Input } from 'antd';
+import { Typography, Modal, Form, Input, Alert, notification } from 'antd';
+import { ethers } from 'ethers';
+import Web3 from 'web3';
 
 import { PaymentAPI } from '../../api';
 import { useAuth, useMetamask } from '../../hooks';
 import { ethereum } from '../../constants';
+import { ERC20 } from '../../api/erc20';
 
 interface IModalProps {
   children: React.ReactNode;
 }
 
 const DEFAULT_CREDITS = 50;
+const DAY_ADDRESS = '0xe814aee960a85208c3db542c53e7d4a6c8d5f60f';
+const web3 = new Web3(ethereum as any);
+const dayContract = new web3.eth.Contract(ERC20 as any, DAY_ADDRESS);
 
 function BuyCreditsModal({ children }: IModalProps) {
   const [form] = Form.useForm();
   const { user } = useAuth();
+  const { changeNetwork, connect } = useMetamask();
   const [visible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [credits, setCredits] = useState(DEFAULT_CREDITS);
   const [paymentAddress, setPaymentAddress] = useState('');
+  const [paymentComplete, setPaymentComplete] = useState(false);
 
   const handleOpenModal = useCallback(() => setVisible(true), []);
 
-  const handleCloseModal = useCallback(() => setVisible(false), []);
+  const handleCloseModal = useCallback(() => {
+    setVisible(false);
+    setPaymentComplete(false);
+    setCredits(DEFAULT_CREDITS);
+    form.resetFields();
+  }, [form]);
 
   const handleSubmit = useCallback(async () => {
     await form.validateFields();
-    await ethereum.request({
-      method: 'wallet_switchEthereumChain',
-      params: [{ chainId: '0x1' }],
-    });
-    const [address] = (await ethereum.request<[string]>({ method: 'eth_requestAccounts' }))!;
-    console.log(address);
-    await PaymentAPI.initialize(user.apiKey, address!);
-  }, [form, user?.apiKey]);
+    setLoading(true);
+    try {
+      const { account } = await connect();
+      await changeNetwork(1);
+      await PaymentAPI.initialize(user.apiKey, account!);
+      const amountDay = ethers.utils.parseEther(`${credits}`);
+      await dayContract.methods.transfer(paymentAddress, amountDay).send({ from: account });
+      setPaymentComplete(true);
+      notification.success({
+        message: 'Payment successful',
+      });
+    } catch (e) {
+      console.error(e);
+      notification.error({ message: (e as any).message || 'Error. Please try again.' });
+    } finally {
+      setLoading(false);
+    }
+  }, [changeNetwork, connect, credits, form, paymentAddress, user?.apiKey]);
 
   useEffect(() => {
     PaymentAPI.getPaymentAddress().then((addr) => setPaymentAddress(addr));
@@ -46,6 +69,8 @@ function BuyCreditsModal({ children }: IModalProps) {
       </div>
       <Modal
         visible={visible}
+        destroyOnClose
+        okButtonProps={{ disabled: loading, loading }}
         title="Buy Credits"
         okText="Buy"
         cancelText="Close"
@@ -54,7 +79,7 @@ function BuyCreditsModal({ children }: IModalProps) {
       >
         <ModalContent>
           <div className="description">
-            Use the form below to make a payment in{' '}
+            You can buy credits with the{' '}
             <Typography.Link
               href="https://coinmarketcap.com/currencies/chronologic/"
               target="_blank"
@@ -62,23 +87,41 @@ function BuyCreditsModal({ children }: IModalProps) {
             >
               DAY token
             </Typography.Link>
-            .<br /> Your credits will be added to your account within a couple minutes.
+            .<br />
+            Specify the amount of credits and click "Buy".
+            <br />
           </div>
-          <Form form={form}>
+          <Form form={form} initialValues={{ credits: DEFAULT_CREDITS }}>
             <div className="creditsWrapper">
               <Form.Item
+                className="creditsFormItem"
                 name="credits"
-                initialValue={DEFAULT_CREDITS}
-                rules={[
-                  { required: true, message: 'Please specify the amount of credits' },
-                  { min: 1, message: 'Amount must be at least 1' },
-                ]}
+                rules={[{ required: true, message: 'Please specify the amount of credits' }]}
               >
-                <Input type="number" min="1" step="1" onChange={(e) => setCredits(Number(e.target.value))} />
+                <Input
+                  type="number"
+                  min="1"
+                  step="1"
+                  disabled={loading}
+                  onChange={(e) => setCredits(Number(e.target.value))}
+                />
               </Form.Item>
               <div className="calculation">Credits = {credits} DAY</div>
             </div>
           </Form>
+          <Alert
+            message="Use only this form to buy credits to ensure that your payment is processed correctly"
+            type="warning"
+            showIcon
+          />
+          {paymentComplete && (
+            <div>
+              <br />
+              Thank you for your payment! 🎉
+              <br /> Your credits will be added to your account within a couple minutes.
+              <br /> <i>Don't forget to switch back to Automate Network in Metamask 🚀</i>
+            </div>
+          )}
         </ModalContent>
       </Modal>
     </Container>
@@ -92,6 +135,21 @@ const ModalContent = styled.div`
     display: flex;
     flex-direction: row;
     color: ${(props) => props.theme.colors.text};
+    justify-content: center;
+  }
+
+  .description {
+    margin-bottom: 24px;
+  }
+
+  .creditsFormItem {
+    width: 100px;
+  }
+
+  .calculation {
+    color: ${(props) => props.theme.colors.text};
+    margin-left: 8px;
+    margin-top: 4px;
   }
 `;
 
